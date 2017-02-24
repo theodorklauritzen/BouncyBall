1dc65f3e2eddc25e84e6ec3d0174afed0aa49415
Theodor Kvalsvik Lauritzen <Theodor Kvalsvik Lauritzen>
2017-02-24 19:10:00 +0100

    Added an index.html file with reference to the p5.js library and to my script file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-  res.send("/");
-});
+// router.get("/", function(req, res, next) {
+//   res.send("/");
+// });
 
 module.exports = router;
