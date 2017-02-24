0b5db03c40d547db61dbecc71bea15a472368026
Theodor Kvalsvik Lauritzen <Theodor Kvalsvik Lauritzen>
2017-02-24 19:57:39 +0100

    End of today's job, this commit contains many bugs, please use the erlier one.

--- public/script.js
+++ public/script.js
@@ -1,10 +1,44 @@
 
 var socket = io.connect("http://localhost:3000/");
 
+var runCode = function(socket) {
+  background(50);
+  fill(255);
+  noStroke();
+
+  textSize(50);
+  textAlign(CENTER);
+  text("Please Wait...", width / 2, height / 2 - 50 / 2);
+}
+
+var url = "";
+function setUrl(newUrl) {
+
+  var runned = false;
+  socket.on("getCode" + newUrl, function (data) {
+    if(!runned) {
+      runned = true;
+      if(data.url) {
+        url = data.url;
+        runCode = data.draw;
+        data.setup(socket);
+      }
+    }
+  });
+
+  socket.emit("getCode", newUrl);
+
+  setTimeout(function () {
+    console.error("the code would not load.  The timeout was on 500 ms.");
+  }, 500);
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  setUrl("/startup");
 }
 
 function draw() {
-  background(50);
+  runCode(socket);
 }
--- socket.js
+++ socket.js
@@ -4,5 +4,38 @@ module.exports = function(server) {
 
   io.sockets.on("connection", function(socket) {
     console.log("new connection");
+
+    //laod code
+    socket.on("getCode", function(data) {
+      var paths = {};
+
+      paths["/startup"] = parseCode(function(socket) {
+        console.log("setup");
+      }, function(socket) {
+        console.log("draw");
+      });
+
+      console.log(paths.hasOwnProperty(data));
+
+      if(paths.hasOwnProperty(data)) {
+        var send = paths[data];
+        send.url = data;
+        socket.emit("getCode" + data, send)
+      } else {
+        socket.emit("getCode" + data, {
+          url: null,
+          setup: null,
+          draw: null
+        });
+      }
+
+      function parseCode(setup, draw) {
+        var ret = {
+          setup: setup,
+          draw: draw
+        };
+        return ret;
+      }
+    });
   });
 }
