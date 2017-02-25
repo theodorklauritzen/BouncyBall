eb78267110488dbed169660a1ea6502fb01018e1
Theodor Kvalsvik Lauritzen <Theodor Kvalsvik Lauritzen>
2017-02-25 13:10:47 +0100

    Fixing the window resize bug that apperared in the latest merge

--- public/script.js
+++ public/script.js
@@ -1,52 +1,6 @@
 
-var socket = io.connect("http://localhost:3000/");
 
-var runCode = function(socket) {
-  background(50);
-  fill(255);
-  noStroke();
 
-  textSize(50);
-  textAlign(CENTER);
-  text("Please Wait...", width / 2, height / 2 - 50 / 2);
-}
-
-var url = "";
-function setUrl(newUrl) {
-
-  socket.emit("getCode", newUrl);
-
-  setTimeout(function () {
-    if(url != newUrl) {
-      console.error("the code would not load.  The timeout was on 500 ms.");
-    }
-  }, 500);
-}
-
-<<<<<<< HEAD
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  socket.on("getCode", function (data) {
-    //console.log(data);
-    if(data.url) {
-      url = data.url;
-      eval(data.draw);
-      runCode = Sdraw;
-      eval(data.setup);
-      Ssetup();
-    }
-  });
-
-  setUrl("/startup");
-}
-
-function draw() {
-  runCode();
-}
-
-=======
->>>>>>> reload
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
