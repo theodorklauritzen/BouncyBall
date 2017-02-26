697f3b690e4233b794f153af6d33177a70468cd5
xupertheo <xupertheo@hotmail.com>
2017-02-26 18:36:33 +0100

    added a ball image and a font
    I've added a ball image(I created it my self 😁), and a font i found on
    the internet.  I also tried to laod it in /home/

--- public/home/script.js
+++ public/home/script.js
@@ -1,8 +1,20 @@
 
+var ballImg;
+var font;
 
+function preload() {
+  ballImg = loadImage("../data/img/ball.png");
+
+  // font: comic sans
+  //       WTTWmesssy - by What the Teacger Wants
+  // http://www.fontspace.com/what-the-teacher-wants/wttwmessy
+  font = loadFont("../data/font/WTTWmessy.ttf");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  textFont(font, 36);
 }
 
 function draw() {
@@ -12,4 +24,9 @@ function draw() {
   textSize(50);
   textAlign(CENTER);
   text("Home", width / 2, height / 2 - 25);
+
+  fill(255, 0, 0);
+  noStroke();
+  ellipse(ballImg.width / 2, ballImg.height / 2, 240 - 2, 240 - 2);
+  image(ballImg, 0, 0);
 }
