21fa7be6fd8e0332223d1d646f472dd38ce79600
xupertheo <xupertheo@hotmail.com>
2017-02-27 16:08:41 +0100

    Added a easier way to laod images.
    You can now use the function loadImg and pass in the name of the image.
    (Don't say the file or the path).

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,10 @@ var img = {};
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+function loadImg(name) {
+  var startPath = "../data/img/";
+  var endPath = ".png"
+  var path = startPath + name + endPath;
+  img[name] = loadImage(path);
+}
