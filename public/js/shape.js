13fb6f16c531a4692accab7b2916945b93c9f45c
xupertheo <xupertheo@hotmail.com>
2017-03-04 18:20:46 +0100

    hing to another computer, see last commit

--- public/js/shape.js
+++ public/js/shape.js
@@ -1,6 +1,21 @@
 function Shape(data) {
   this.type = data.type;
 
+  // change the corners to the value goes from 0 to 1000
+  var stop = false;
+  while(!stop) {
+    console.log(width && height);
+    if(width && height) {
+      for(var i = 0; i < data.corners.length; i++) {
+        data.corners[i] = {
+          x: data.corners[i].x * width / 1000,
+          y: data.corners[i].y * height / 1000
+        }
+      }
+      stop = true;
+    }
+  }
+
   this.corners = [];
 
   if(this.type === "shape") {
@@ -21,8 +36,7 @@ function Shape(data) {
 
 Shape.prototype.show = function() {
   fill(150);
-  strokeWeight(1);
-  stroke(0);
+  noStroke()
   beginShape()
   for (var i = 0; i < this.corners.length; i++) {
     vertex(this.corners[i].x, this.corners[i].y);
